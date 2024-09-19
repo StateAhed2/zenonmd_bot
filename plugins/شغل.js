fa4d16a79e81b8b1cdce77a4d70592a8de56cab3
StateAhed2 <rjlaldwlhahdahd@gmail.com>
2024-09-19 20:02:36 +0300

    Update شغل.js

--- plugins/شغل.js
+++ plugins/شغل.js
@@ -1,159 +1,166 @@
 import fetch from 'node-fetch';
-import yts from 'yt-search';
-import ytdl from 'ytdl-core';
-import axios from 'axios';
-import { youtubedl, youtubedlv2 } from '@bochilteam/scraper';
-import { prepareWAMessageMedia, generateWAMessageFromContent } from '@whiskeysockets/baileys';
+import { prepareWAMessageMedia, generateWAMessageFromContent, getDevice } from 'baileys';
 
-const handler = async (m, { command, usedPrefix, conn, args, text }) => {
-  if (command === 'شغل') {
-    if (!text) throw *❲ ❗ ❳ يرجي إدخال نص للبحث في يوتيوب .*\nمثال :\n> ➤  ${usedPrefix + command} القرآن الكريم\n> ➤  ${usedPrefix + command} https://youtu.be/JLWRZ8eWyZo?si=EmeS9fJvS_OkDk7p;
+let enviando = false;
+
+const handler = async (m, { command, usedPrefix, conn, text }) => {
+  const device = await getDevice(m.key.id);
+
+  if (device === 'desktop' || device === 'web') {
+    throw `*[❗] الرسائل ذات الأزرار غير متاحة على WhatsApp web، يرجى استخدام الهاتف المحمول لعرض واستخدام الرسائل ذات الأزرار.*`;
+  }
+
+  if (enviando) return;
+  enviando = true;
+
+  if (command === 'مشغل') {
+    if (!text) {
+      conn.sendMessage(m.chat, {
+        text: `*❲ ❗ ❳ يرجى إدخال نص للبحث في يوتيوب.*\nمثال :\n> ➤ ${usedPrefix + command} القرآن الكريم\n> ➤ ${usedPrefix + command} https://youtu.be/JLWRZ8eWyZo?si=EmeS9fJvS_OkDk7p`
+      }, { quoted: m });
+      await conn.sendMessage(m.chat, { react: { text: '❗', key: m.key } });
+      enviando = false;
+      return;
+    }
+
+    const isYouTubeLink = await isValidYouTubeLink(text);
+    await conn.sendMessage(m.chat, { react: { text: '🔍', key: m.key } });
 
     try {
-      const yt_play = await search(args.join(' '));
-      const dataMessage = *❲ نتيجة البحث عن : ${text} ❳*\n➤ العنوان : ${yt_play[0].title}\n➤ النشر : ${yt_play[0].ago}\n➤ الطول : ${secondString(yt_play[0].duration.seconds)}\n➤ الرابط : ${yt_play[0].url}\n➤ المشاهدات : ${MilesNumber(yt_play[0].views)}\n➤ الصانع : ${yt_play[0].author.name}\n➤ القناة : ${yt_play[0].author.url}.trim();
+      const apiUrls = isYouTubeLink
+        ? [
+            `https://api.cafirexos.com/api/ytinfo?url=${text}`,
+            `https://api-brunosobrino-koiy.onrender.com/api/ytinfo?url=${text}&apikey=BrunoSobrino`,
+            `https://api-brunosobrino-dcaf9040.koyeb.app/api/ytinfo?url=${text}`
+          ]
+        : [
+            `https://api.cafirexos.com/api/ytplay?text=${text}`,
+            `https://api-brunosobrino.onrender.com/api/ytplay?text=${text}&apikey=BrunoSobrino`,
+            `https://api-brunosobrino-dcaf9040.koyeb.app/api/ytplay?text=${text}`
+          ];
 
-      const iturl = yt_play[0].url;
-      const itimg = yt_play[0].thumbnail;
-      const messa = await prepareWAMessageMedia({ image: { url: itimg } }, { upload: conn.waUploadToServer });
+      
 
-      let msg = generateWAMessageFromContent(m.chat, {
+      const results = await Promise.all(
+        apiUrls.map(url => fetch(url).then(res => res.json()).catch(err => null))
+      );
+
+      const data = results.find(result => result && result.resultado && result.resultado.url);
+      
+      if (!data) {
+        await handleError(m, usedPrefix, command);
+        return;
+      }
+
+await conn.sendMessage(m.chat, { react: { text: '⏳', key: m.key } });
+
+      const dataMessage = `*❲ نتيجة البحث عن: ${text} ❳*\n\n➤ العنوان: ${data.resultado.title}\n➤ النشر: ${data.resultado.publicDate}\n➤ الرابط: ${data.resultado.url}\n➤ القناة: ${data.resultado.channel}`.trim();
+
+      const mediaMessage = await prepareWAMessageMedia({ image: { url: data.resultado.image } }, { upload: conn.waUploadToServer });
+      const msg = generateWAMessageFromContent(m.chat, {
         viewOnceMessage: {
           message: {
             interactiveMessage: {
               body: { text: dataMessage },
-              footer: { text: ©${global.wm}.trim() },
+              footer: { text: `${global.wm}`.trim() },
               header: {
                 hasMediaAttachment: true,
-                imageMessage: messa.imageMessage,
+                imageMessage: mediaMessage.imageMessage,
               },
               nativeFlowMessage: {
                 buttons: [
-                  { name: 'quick_reply', buttonParamsJson: JSON.stringify({ display_text: 'صوتي', id: ${usedPrefix}mp3.1 ${iturl} }) },
-                  { name: 'quick_reply', buttonParamsJson: JSON.stringify({ display_text: 'ملف صوتي', id: ${usedPrefix}mp3.2 ${iturl} }) },
-                  { name: 'quick_reply', buttonParamsJson: JSON.stringify({ display_text: 'ريكورد', id: ${usedPrefix}mp3.3 ${iturl} }) },
-                  { name: 'quick_reply', buttonParamsJson: JSON.stringify({ display_text: 'فيديو', id: ${usedPrefix}mp4.1 ${iturl} }) },
-                  { name: 'quick_reply', buttonParamsJson: JSON.stringify({ display_text: 'ملف فيديو', id: ${usedPrefix}mp4.2 ${iturl} }) },
-                  { name: 'quick_reply', buttonParamsJson: JSON.stringify({ display_text: 'جيف', id: ${usedPrefix}mp4.3 ${iturl} }) },
+                  {
+                    name: 'quick_reply',
+                    buttonParamsJson: JSON.stringify({
+                      display_text: 'AUDIO',
+                      id: `${usedPrefix}play.1 ${data.resultado.url}`
+                    })
+                  },
+                  {
+                    name: 'quick_reply',
+                    buttonParamsJson: JSON.stringify({
+                      display_text: 'VIDEO',
+                      id: `${usedPrefix}play.2 ${data.resultado.url}`
+                    })
+                  }
                 ],
-                messageParamsJson: "",
-              },
-            },
-          },
-        },
+                messageParamsJson: '',
+              }
+            }
+          }
+        }
       }, { userJid: conn.user.jid, quoted: m });
 
       await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id });
-
-    } catch {
-      throw *❲ ❗ ❳ حدث خطأ عند البحث في يوتيوب .*\nيرجي ادخال نص صحيح أو رابط مثال :\n> ➤  ${usedPrefix + command} القرآن الكريم\n> ➤  ${usedPrefix + command} https://youtu.be/JLWRZ8eWyZo?si=EmeS9fJvS_OkDk7p;
-    }
-
-  } else if (command.startsWith('mp3.')) {
-    try {
-      const q = '128kbps';
-      const v = text;
-      const yt = await youtubedl(v).catch(async () => await youtubedlv2(v));
-      const dl_url = await yt.audio[q].download();
-      const ttl = await yt.title;
-
-      if (command === 'mp3.1') {
-        await conn.sendMessage(m.chat, { audio: { url: dl_url }, mimetype: 'audio/mpeg', fileName: ${ttl}.mp3 }, { quoted: m });
-      } else if (command === 'mp3.2') {
-        await conn.sendMessage(m.chat, { document: { url: dl_url }, mimetype: 'audio/mpeg', fileName: ${ttl}.mp3 }, { quoted: m });
-      } else if (command === 'mp3.3') {
-        await conn.sendMessage(m.chat, { audio: { url: dl_url }, mimetype: 'audio/ogg; codecs=opus', ptt: true, fileName: ${ttl}.opus }, { quoted: m });
-      }
-
+      await conn.sendMessage(m.chat, { react: { text: '✔️', key: m.key } });
     } catch (error) {
-      await handleFallback(command, text, m, conn);
+      console.error(error);
+      await handleError(m, usedPrefix, command);
+    } finally {
+      enviando = false;
     }
-
-  } else if (command.startsWith('mp4.')) {
-    try {
-      const qu = '360';
-      const q = ${qu}p;
-      const v = text;
-      const yt = await youtubedl(v).catch(async () => await youtubedlv2(v));
-      const dl_url = await yt.video[q].download();
-      const ttl = await yt.title;
-      const size = await yt.video[q].fileSizeH;
-
-      if (command === 'mp4.1') {
-        await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: ➤ العنوان : ${ttl}\n➤ الحجم : ${size} }, { quoted: m });
-      } else if (command === 'mp4.2') {
-        await conn.sendMessage(m.chat, { document: { url: dl_url }, fileName: ${ttl}.mp4, mimetype: 'video/mp4', caption: ➤ العنوان : ${ttl}\n➤ الحجم : ${size}, thumbnail: await fetch(yt.thumbnail) }, { quoted: m });
-      } else if (command === 'mp4.3') {
-        await conn.sendMessage(m.chat, { video: { url: dl_url }, gifPlayback: true, caption: ➤ العنوان : ${ttl}\n➤ الحجم : ${size} }, { quoted: m });
-      }
-
-    } catch (error) {
-      await handleFallback(command, text, m, conn);
-    }
+  } else if (command === 'play.1') {
+    await handlePlayCommand(m, text, 'audio', 'error.mp3');
+    
+  } else if (command === 'play.2') {
+    await handlePlayCommand(m, text, 'video', 'error.mp4');
+    
+  } else {
+    throw `*❲ ❗ ❳ أمر غير معروف. يرجى إدخال أمر صحيح.*\nمثال :\n> ➤ ${usedPrefix}مشغل القرآن الكريم\n> ➤ ${usedPrefix}مشغل https://youtu.be/JLWRZ8eWyZo?si=EmeS9fJvS_OkDk7p`;
   }
 };
 
-handler.command = /^(شغول|mp3.1|mp3.2|mp3.3|mp4.1|mp4.2|mp4.3)$/i;
-export default handler;
-
-async function search(query, options = {}) {
-  const search = await yts.search({ query, hl: 'ar', gl: 'AR', ...options });
-  return search.videos;
+async function isValidYouTubeLink(link) {
+  const urlPattern = /^(https?:\/\/)?(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9_-]{11})$/;
+  return urlPattern.test(link);
 }
 
-function MilesNumber(number) {
-  const exp = /(\d)(?=(\d{3})+(?!\d))/g;
-  const rep = '$1.';
-  const arr = number.toString().split('.');
-  arr[0] = arr[0].replace(exp, rep);
-  return arr[1] ? arr.join('.') : arr[0];
-}
+async function handlePlayCommand(m, text, mimeType, fileName) {
 
-function secondString(seconds) {
-  seconds = Number(seconds);
-  const d = Math.floor(seconds / (3600 * 24));
-  const h = Math.floor((seconds % (3600 * 24)) / 3600);
-  const m = Math.floor((seconds % 3600) / 60);
-  const s = Math.floor(seconds % 60);
-  const dDisplay = d > 0 ? d + (d == 1 ? ' día, ' : ' días, ') : '';
-  const hDisplay = h > 0 ? h + (h == 1 ? ' hora, ' : ' horas, ') : '';
-  const mDisplay = m > 0 ? m + (m == 1 ? ' minuto, ' : ' minutos, ') : '';
-  const sDisplay = s > 0 ? s + (s == 1 ? ' segundo' : ' segundos') : '';
-  return dDisplay + hDisplay + mDisplay + sDisplay;
-}
+  await conn.sendMessage(m.chat, { react: { text: '⏳', key: m.key } });
+  
+  try {
+    const apiUrls = [
+      `https://api.cafirexos.com/api/v1/yt${mimeType === 'audio' ? 'mp3' : 'mp4'}?url=${text}`,
+      `https://api.cafirexos.com/api/v2/yt${mimeType === 'audio' ? 'mp3' : 'mp4'}?url=${text}`,
+      `https://api-brunosobrino.onrender.com/api/v1/yt${mimeType === 'audio' ? 'mp3' : 'mp4'}?url=${text}&apikey=BrunoSobrino`,
+      `https://api-brunosobrino.onrender.com/api/v2/yt${mimeType === 'audio' ? 'mp3' : 'mp4'}?url=${text}&apikey=BrunoSobrino`,
+      `https://api-brunosobrino-dcaf9040.koyeb.app/api/v1/yt${mimeType === 'audio' ? 'mp3' : 'mp4'}?url=${text}`,
+      `https://api-brunosobrino-dcaf9040.koyeb.app/api/v2/yt${mimeType === 'audio' ? 'mp3' : 'mp4'}?url=${text}`
+    ];
 
-function bytesToSize(bytes) {
-  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-  if (bytes === 0) return 'n/a';
-  const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
-  return i === 0 ? ${bytes} ${sizes[i]} : ${(bytes / (1024 ** i)).toFixed(1)} ${sizes[i]};
-}
+    
+    const results = await Promise.all(
+      apiUrls.map(url => fetch(url).then(res => res.buffer()).catch(err => null))
+    );
 
-async function ytMp3(url) {
-  return new Promise((resolve, reject) => {
-    ytdl.getInfo(url).then(async (getUrl) => {
-      const result = [];
-      for (let i = 0; i < getUrl.formats.length; i++) {
-        const item = getUrl.formats[i];
-        if (item.mimeType === 'audio/webm; codecs="opus"') {
-          const { contentLength } = item;
-          const bytes = await bytesToSize(contentLength);
-          result[i] = { audio: item.url, size: bytes };
-        }
-      }
-      const fix = result.filter((x) => x.audio !== undefined && x.size !== undefined);
-      resolve(fix[0]);
-    }).catch(reject);
-  });
-}
+    const buff = results.find(result => result);
 
-async function handleFallback(command, text, m, conn) {
-  const isMp3 = command.startsWith('mp3.');
-  const result = await ytMp3(text);
+    if (!buff) {
+      await handleError(m, usedPrefix, 'مشغل');
+      return;
+    }
 
-  if (isMp3) {
-    await conn.sendMessage(m.chat, { audio: { url: result.audio }, fileName: audio.mp3, mimetype: 'audio/mpeg', ptt: false, fileLength: result.size }, { quoted: m });
-  } else {
-    await conn.sendMessage(m.chat, { video: { url: result.audio }, fileName: video.mp4, mimetype: 'video/mp4', fileLength: result.size }, { quoted: m });
+    await conn.sendMessage(m.chat, {
+      [mimeType]: buff,
+      mimetype: mimeType === 'audio' ? 'audio/mpeg' : 'video/mp4',
+      fileName
+    }, { quoted: m });
+    await conn.sendMessage(m.chat, { react: { text: '✔️', key: m.key } });
+  } catch (error) {
+    console.error(error);
+    await handleError(m, usedPrefix, 'مشغل');
+  } finally {
+    enviando = false;
   }
 }
+
+async function handleError(m, usedPrefix, command) {
+  await conn.sendMessage(m.chat, {
+    text: `*❲ ❗ ❳ حدث خطأ عند البحث في يوتيوب.*\nيرجى إدخال نص صحيح أو رابط مثال :\n> ➤ ${usedPrefix}${command} القرآن الكريم\n> ➤ ${usedPrefix}${command} https://youtu.be/JLWRZ8eWyZo?si=EmeS9fJvS_OkDk7p`
+  }, { quoted: m });
+  await conn.sendMessage(m.chat, { react: { text: '❌', key: m.key } });
+}
+
+handler.command = /^(play.1|play.2|مشغل)$/i;
+export default handler;
